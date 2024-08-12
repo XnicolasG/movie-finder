66af0ffbae5b30c8a6ef3f2457b5d9ac8a9c097f
Nicolás García <nicolasg.ngl@gmail.com>
2024-08-12 16:48:07 -0500

    create a custome hook to manage possible error with the inút search, also add the function to collect the value on it

---
+++ src/Hooks/useSearch.js
@@ -0,0 +1,24 @@
+import { useEffect, useRef, useState } from "react"
+
+export const useSearch = () => {
+    const [search, setSearch] = useState()
+    const [error, setError] = useState()
+    const isFirstInput = useRef(true)
+    
+    useEffect(() => {
+      if (isFirstInput.current) {
+        //si search es vacio sera true, si no es vacio sera false
+        isFirstInput.current = search === '' 
+        return
+      };
+      if (search === '') {
+          setError('No se puede buscar una pelicula sin nombre')
+          return
+      };
+      if (search?.match(/^\d+$/)) {
+          setError('No se puede buscar una pelicual con un numero')
+          return
+      };
+    }, [search])
+      return {search, setSearch, error}
+  }
--- src/app.jsx
+++ src/app.jsx
@@ -1,22 +1,37 @@
-import './app.css'
+
 import { Movies } from './components/Movies';
 import { useMovies } from './Hooks/useMovies';
-import withResults from './mocks/withResults.json'
+import './app.css'
+import { useSearch } from './Hooks/useSearch';
+
 
 export const App = () => {
-    const { movies} = useMovies()
+    const { movies } = useMovies();
+    const {search, setSearch, error} = useSearch()
 
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(search);
+    }
+
+   const handleChange =(e)=>{
+    const newQuery = e.target.value
+    if (newQuery.startsWith(' ')) return
+    setSearch(newQuery)
+    
+   }
     return (
         <div className='page'>
             <header className='page_cont'>
                 <h1 className='page_title'>Movie Finder</h1>
-                <form className="page_form">
-                    <input type="text" placeholder='Harry Potter, Matrix, etc..' />
+                <form className="page_form" onSubmit={handleSubmit}>
+                    <input onChange={handleChange} value={search} name='inputField' type="text" placeholder='Harry Potter, Matrix, etc..' />
                     <button>Buscar</button>
                 </form>
+                {error && <p>{error}</p>}
             </header>
             <main>
-                <Movies movies={ movies } />
+                <Movies movies={movies} />
             </main>
         </div>
     )
